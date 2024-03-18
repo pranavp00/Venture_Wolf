471b9f42d7893878189579b04c13f2a98dfeeedc
Pranav Anil Pillai <ppillai294@gmail.com>
2024-03-19 00:31:10 +0530

    made changes in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import {  Row, Col, Stack } from 'react-bootstrap';
 import './footer.css';
 import logo from '../assets/images/footer-logo.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram,faFacebook,faXTwitter,faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram,faFacebook,faXTwitter,faYoutube, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 const Footer = () => {
@@ -14,19 +14,20 @@ const Footer = () => {
 
     <footer className="footer bg-dark text-white w-100">
       
-        <Row className="justify-content-between align-items-start row row-cols-1 row-cols-md-2">
+        <Row className="align-items-start row row-cols-1 row-cols-md-2">
           <Col md={3} className="d-flex text-left logo-col">
             <Stack className = 'stack'>
                 <img src={logo} alt="Your Logo" className="logo" />
                 <div className="mail-icon"><a href = '#'><FontAwesomeIcon icon={faEnvelope} /> funding@venturewolf</a></div>
-                <div className="mail-icon">
+                {/* <div className="icon">
                     <Stack direction="horizontal" className="justify-content-between">
                         <div className="icons"><a href='#'><FontAwesomeIcon icon={faInstagram} /></a></div>
                         <div className="icons"><a href='#'><FontAwesomeIcon icon={faFacebook} /></a></div>
                         <div className="icons"><a href='#'><FontAwesomeIcon icon={faYoutube} /></a></div>
                         <div className="icons"><a href='#'></a><FontAwesomeIcon icon={faXTwitter} /></div>
+                        <div className="icons"><a href='#'></a><FontAwesomeIcon icon={faLinkedinIn} /></div>
                     </Stack>
-                </div>
+                </div> */}
             </Stack>
           </Col>
           <Col md={1} className="sitemaps">
@@ -49,11 +50,11 @@ const Footer = () => {
             <li><a href="#">Our Values</a></li>
             </ul>
           </Col>
-          <Col md={1} className="sitemaps">
+          {/* <Col md={1} className="sitemaps">
             <ul className="list-unstyled ">
               <li><a href="#">Gallery</a></li>
             </ul>
-          </Col>
+          </Col> */}
           <Col md={1} className="sitemaps">
             <ul className="list-unstyled ">
               <li><a href="#">Contact</a></li>
@@ -63,6 +64,15 @@ const Footer = () => {
           
           
         </Row>
+        <div className="icon">
+                    <Stack direction="horizontal" className="">
+                        <div className="icons"><a href='#'><FontAwesomeIcon icon={faInstagram} /></a></div>
+                        <div className="icons"><a href='#'><FontAwesomeIcon icon={faFacebook} /></a></div>
+                        <div className="icons"><a href='#'><FontAwesomeIcon icon={faYoutube} /></a></div>
+                        <div className="icons"><a href='#'></a><FontAwesomeIcon icon={faXTwitter} /></div>
+                        <div className="icons"><a href='#'></a><FontAwesomeIcon icon={faLinkedinIn} /></div>
+                    </Stack>
+                </div>
       
       <div className="text-center  copyright" >
         © {new Date().getFullYear()} Copyright:<a className="" href="#"> Venture Wolf</a>
